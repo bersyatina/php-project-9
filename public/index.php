1d1efba21584132918a61f732dc9cb06f9572839
Apanasenko Andrey <aw_apanasenko@mail.ru>
2023-02-06 11:51:55 +0000

    added pdo

--- public/index.php
+++ public/index.php
@@ -4,10 +4,19 @@ use DI\Container;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
+use Hexlet\Code\Connection;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+try {
+    Connection::get()->connect();
+    echo 'A connection to the PostgreSQL database sever has been established successfully.';
+} catch (\PDOException $e) {
+    echo $e->getMessage();
+}
+
 $container = new Container();
+
 AppFactory::setContainer($container);
 
 $container->set('view', function() {
@@ -16,10 +25,18 @@ $container->set('view', function() {
 
 $app = AppFactory::create();
 
+//$container->db = function ($c) {
+//    $db = $c['settings']['db'];
+//
+//    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+//    return $pdo;
+//};
+
 $app->add(TwigMiddleware::createFromContainer($app));
 
 $app->get('/', function ($request, $response, $args) {
     return $this->get('view')->render($response, 'face.twig', []);
-})->setName('profile');
+})->setName('face');
 
 $app->run();
