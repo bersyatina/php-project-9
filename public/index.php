19c4dd87c481f454be45a3483f042926d5d3c940
Apanasenko Andrey <aw_apanasenko@mail.ru>
2023-02-06 06:57:29 +0000

    added bootstrap framework in project

--- public/index.php
+++ public/index.php
@@ -1,22 +1,27 @@
 <?php
-
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+error_reporting(-1); ini_set('display_errors', 'On');
+use DI\Container;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+AppFactory::setContainer($container);
+
+$container->set('view', function() {
+    return Twig::create('templates', ['cache' => false]);
+});
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("Hello, Bers");
-    return $response;
-});
+$app->add(TwigMiddleware::createFromContainer($app));
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
+$app->get('/', function ($request, $response, $args) {
+    return $this->get('view')->render($response, 'face.twig', [
+        'name' => $args['name']
+    ]);
+})->setName('profile');
 
 $app->run();
