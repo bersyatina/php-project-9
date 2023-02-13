a6c3bf021dfe9d72a0077d40362e28434c4eed38
Apanasenko Andrey <aw_apanasenko@mail.ru>
2023-02-13 13:59:02 +0000

    added flash messages in pages

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ use Hexlet\Code\PostgreSQLCreateTable;
 use Hexlet\Code\PostgreSQLGetUrls;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
+use Slim\Flash;
 use Slim\Views\TwigMiddleware;
 use Hexlet\Code\Connection;
 
@@ -48,6 +49,7 @@ $container->set('view', function() {
     return $twig;
 });
 
+
 $app = AppFactory::create();
 
 $app->add(TwigMiddleware::createFromContainer($app));
@@ -75,13 +77,14 @@ $app->post('/urls', function (Request $request, Response $response) use ($app) {
         ]);
     }
 
-    $site = new PostgreSQLGetUrls($connection);
-    $site = $site->getUrl($result['success']['id']);
+    $getterObject = new PostgreSQLGetUrls($connection);
+    $site = $getterObject->getUrl($result['success']['id']);
+    $checks = $getterObject->getChecks($result['success']['id']);
 
     return $this->get('view')->render($response, 'url.twig', [
-        'flash' => $result,
+        'flash' => ['success' => $result['success']['message']],
         'site' => $site,
-        'checks' => [],
+        'checks' => $checks,
     ]);
 })->setName('face');
 
@@ -89,6 +92,7 @@ $app->get('/urls/{id}', function ($request, $response, $args) {
     $getterObject = new PostgreSQLGetUrls(Connection::get()->connect());
 
     $site = $getterObject->getUrl($args['id']);
+    $site[]
     $checks = $getterObject->getChecks($args['id']);
 
     return $this->get('view')->render($response, 'url.twig', [
@@ -126,12 +130,14 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) {
 
     $site = $getterObject->getUrl($args['url_id']);
 
+    $flash = ['errors' => 'Произошла ошибка при проверке, не удалось подключиться'];
+
     if (Handler::isDomainAvailible($site['name'])) {
         try {
             $clientGuzzle = new GuzzleHttp\Client(['base_uri' => $site['name']]);
             $requestCheck = $clientGuzzle->request('GET') ?? false;
         } catch (\PDOException $e) {
-            echo $e->getMessage();
+            $flash = ['errors' => $e->getMessage()];
         }
 
         if (isset($requestCheck)) {
@@ -158,13 +164,15 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) {
             ];
 
             $inserter->addCheck($pageData);
+
+            $flash = ['success' => 'Страница успешно проверена'];
         }
     }
 
     $checks = $getterObject->getChecks($site['id']);
 
     return $this->get('view')->render($response, 'url.twig', [
-        'flash' => [],
+        'flash' => $flash,
         'site' => $site,
         'checks' => $checks,
     ]);
